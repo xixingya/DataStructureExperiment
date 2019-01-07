548db4dce5e18e7cdb3dd2a89bb16b90d3ca6993
xixingya <37765666+xixingya@users.noreply.github.com>
2019-01-07 10:33:36 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/DataStructureExperiment.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructureExperiment.dir/main.cpp.obj"
+  "DataStructureExperiment.pdb"
+  "DataStructureExperiment.exe"
+  "DataStructureExperiment.exe.manifest"
+  "libDataStructureExperiment.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructureExperiment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
