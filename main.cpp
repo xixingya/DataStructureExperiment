0355e739abca9ec613d41777aedec12cb30be7b4
xixingya <37765666+xixingya@users.noreply.github.com>
2019-01-08 12:44:34 +0800

    question 2 is OK

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int num=0;
 int right=0;
 *//*int n=0;
 int k=0;*/
+char str[1000];
 int iSDeepthK(char a[],int k){//判断是不是一个深度为k的序列
     int b=strlen(a);
     int countLeft=0;
@@ -47,6 +48,38 @@ int iSDeepthK(char a[],int k){//判断是不是一个深度为k的序列
 
 }//函数判断深度是不是为k结尾
 int n,k,num,left,right,i,d,max;
+void listKuoHao(int d,char *a,int max);
+void solve(int n, int ls, int rs,int k);
+int main(){
+    char a[21];
+    int k=1;
+    //当k!=0，输入序列并且判断深度是不是为k
+    while(true) {
+        scanf("%d", &k);
+        if (k == 0)
+            break;
+        scanf("%s", a);
+        int iSK = iSDeepthK(a, k);
+        if (iSK == OK)
+            printf("YES\n");
+        else if (iSK == ERROR)
+            printf("NO\n");
+        else if (iSK == NOTSUIT)
+            printf("括号不匹配\n");
+    }
+
+    while(true){
+        printf("please input n and k so that we can list the kuohao\n");
+        scanf("%d %d",&n,&k);
+        if(k>n)
+            break;
+        solve(n,0,0,k);
+
+    }
+
+
+
+}
 void listKuoHao(int d,char *a,int max){
     if(d==2*n){
         if(a[d-1]==')'&&max==k){
@@ -56,11 +89,11 @@ void listKuoHao(int d,char *a,int max){
     }
     if(left<right||(left-right)>k||left>n)//当右括号大于于左括号，或者嵌套深度大于k时
         return;
-    if(max<k&&d+2*max-(left-right)>=2*n)
+    if(max<k&&(d+2*max-(left-right)>=2*n))
         return;
     a[d]='(';
     left++;
-    listKuoHao(d+1,a,max>left-right?max:(left-right));
+    listKuoHao(d+1,a,max>(left-right)?max:(left-right));
     left--;
     a[d]=')';
     right++;
@@ -68,36 +101,16 @@ void listKuoHao(int d,char *a,int max){
     right--;
 }
 
-int main(){
-    char a[100];
-    int k=1;
-    //当k!=0，输入序列并且判断深度是不是为k
-    while(true){
-        scanf("%d",&k);
-        if(k==0)
-            break;
-        scanf("%s",a);
-        int iSK=iSDeepthK(a,k);
-        if(iSK==OK)
-            printf("YES\n");
-        else if(iSK==ERROR)
-            printf("NO\n");
-        else if(iSK==NOTSUIT)
-            printf("括号不匹配\n");
+void solve(int n, int ls, int rs, int k)
+{
+    if(ls == rs && ls + rs == 2*n&&iSDeepthK(str,k)==true) {
+        printf("%s\n", str);
+        return ;
     }
-    a[0]='(';
-    while(true){
-        printf("please input n and k so that we can list the kuohao\n");
-        scanf("%d %d",&n,&k);
-        if(k>n)
-            break;
-        a[2*n]='\0';
-        left=1;num=right=0;
-        listKuoHao(1,a,0);
-        printf("%dkuohao %dqiantao is%d suggestion\n",n,k,num);
-
-    }
-
-
-
+    if(ls < rs || ls + rs >= 2*n) return ;
+    int index = ls + rs;
+    str[index] = '(';
+    solve(n, ls+1, rs,k);
+    str[index] = ')';
+    solve(n, ls, rs+1,k);
 }
