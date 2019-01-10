e62c5f54663e54f50e78987d8e4d13dd33d0fb1a
xixingya <37765666+xixingya@users.noreply.github.com>
2019-01-10 12:15:47 +0800

    three question all have a solution

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #define OK 1
 #define ERROR 0
 #define NOTSUIT 2
+#define MAX_N 30
+int countAll[MAX_N+1][MAX_N+1]={};
 /*int left=1;
 int num=0;
 int right=0;
@@ -50,8 +52,10 @@ int iSDeepthK(char a[],int k){//判断是不是一个深度为k的序列
 int n,k,num,left,right,i,d,max;
 void listKuoHao(int d,char *a,int max);
 void solve(int n, int ls, int rs,int k);
+void solve3(int n, int ls, int rs, int k,int *count);
+void countA(int n,int k);
 int main(){
-    char a[21];
+    char a[100];
     int k=1;
     //当k!=0，输入序列并且判断深度是不是为k
     while(true) {
@@ -66,7 +70,7 @@ int main(){
             printf("NO\n");
         else if (iSK == NOTSUIT)
             printf("括号不匹配\n");
-    }
+    }//first question
 
     while(true){
         printf("please input n and k so that we can list the kuohao\n");
@@ -75,12 +79,43 @@ int main(){
             break;
         solve(n,0,0,k);
 
+    }//second question
+    for(n=1;n<=MAX_N;n++){
+        countAll[n][1]=1;
+        countAll[n][n]=1;
     }
+    for(n=1;n<=MAX_N;n++){
+        for(k=1;k<=n;k++){
+            if(k!=n&&k!=1)
+                countA(n,k);
+        }
+    }
+    while(true){
+        printf("please input n and k so that we can count the kuohao\n");
+        scanf("%d %d",&n,&k);
+        if(k>n)
+            break;
+        printf("%d %d have %d situation\n",n,k,countAll[n][k]);
+
+
+
+    }
+    /*while(true){
+        int *count;
+        *count=0;
+        printf("please input n and k so that we can list the kuohao\n");
+        scanf("%d %d",&n,&k);
+        if(k>n)
+            break;
+        solve3(n,0,0,k,count);
+        printf("%d\n",*count);
+
+    }*/
 
 
 
 }
-void listKuoHao(int d,char *a,int max){
+/*void listKuoHao(int d,char *a,int max){
     if(d==2*n){
         if(a[d-1]==')'&&max==k){
             printf("%d %s\n",++num,a);
@@ -99,11 +134,24 @@ void listKuoHao(int d,char *a,int max){
     right++;
     listKuoHao(d+1,a,max>(left-right)?max:(left-right));
     right--;
+}*/
+void solve3(int n, int ls, int rs, int k,int *count)
+{
+    if(ls == rs && ls + rs == 2*n&&iSDeepthK(str,k)==OK) {
+        (*count)++;
+        return ;
+    }
+    if(ls < rs || ls + rs >= 2*n) return ;
+    int index = ls + rs;
+    str[index] = '(';
+    solve3(n, ls+1, rs,k,count);
+    str[index] = ')';
+    solve3(n, ls, rs+1,k,count);
 }
 
 void solve(int n, int ls, int rs, int k)
 {
-    if(ls == rs && ls + rs == 2*n&&iSDeepthK(str,k)==true) {
+    if(ls == rs && ls + rs == 2*n&&iSDeepthK(str,k)==OK) {
         printf("%s\n", str);
         return ;
     }
@@ -113,4 +161,27 @@ void solve(int n, int ls, int rs, int k)
     solve(n, ls+1, rs,k);
     str[index] = ')';
     solve(n, ls, rs+1,k);
+}
+void countA(int n,int k){
+    int i,j;
+    for (i=k;i<n;i++){
+        int temp=0;
+        for(j=0;j<=n-i;j++){
+            int temp_i=j;
+            int temp_j=n-i-j;
+            int temp_k1=0;
+            int temp_k2=0;
+            if(temp_i>k-1)   temp_i=k-1;//j和k-1的较小值赋给temp_i,最大深度不能大于k-1;
+            if(temp_j>k)     temp_j=k;//n-i-j和k的较小值赋给temp_j,最大深度不能大于k
+            for(;temp_i>0;temp_i--)
+                temp_k1=temp_k1+countAll[j][temp_i];//求和countAll[j][temp_i]
+            for(;temp_j>0;temp_j--)
+                temp_k2=temp_k2+countAll[n-i-j][temp_j];//求和countAll[n-i-j][temp_j]
+            if(temp_k1<1)  temp_k1=1;
+            if(temp_k2<1)  temp_k2=1;
+            temp=temp+temp_k1*temp_k2;//求和temp_k1*temp*k2
+        }
+        countAll[n][k]=countAll[n][k]+countAll[i-1][k-1]*temp;
+    }
+    countAll[n][k]=countAll[n][k]+countAll[n-1][k-1];
 }
